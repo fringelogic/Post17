5e3ae7dd1289640d78a9bef67aed5610890c0f4c
teganstorm <55839174+teganstorm@users.noreply.github.com>
2021-01-19 13:05:00 +0000

    starting to add functionallity to the new post page

--- scripts/channels.js
+++ scripts/channels.js
@@ -1,12 +1,3 @@
-
-
-function newChannel() {
-    // prompt user for channel name
-    // channel tags
-    // channel public or private
-    return 1;
-}
-
 var searchfor;
 function searching() {
     // we need to get the input from the text bar for search to send it to the search page
