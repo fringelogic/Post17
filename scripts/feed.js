15320d6d9bac09702c833c07c9faf290a478860a
teganstorm <55839174+teganstorm@users.noreply.github.com>
2021-01-26 17:58:57 +0000

    an easy option proposal for the feed

--- scripts/feed.js
+++ scripts/feed.js
@@ -4,3 +4,54 @@ function searching() {
     searchfor = document.getElementById("searchinput").value;
     sessionStorage.setItem("search", searchfor);
 }
+
+//require ('connect_db.php');
+/*
+function Post() {
+        
+    this.id = -1;
+    this.img = -1;
+
+        //var ratecount = 0;
+        //var totalrates = 0;
+        // var rates = get the list of rates from the DB
+        // for (rate: rates) {
+        //  totalrates = totalrates + rate
+        //  ratecount++;
+        //var averagerates = totalrates/ratecount;
+    this.averagerate = 0;
+
+    this.description = -1;
+    this.comments = [];
+    this.user = -1;
+    this.tags = [];
+    this.geotag = -1;
+
+    const post = document.createElement("P");
+    var feedbox = document.getElementById("feedbox");
+    feedbox.innerHTML("feedbox");
+        
+    alert("alert");
+
+    const rate = document.createElement("P");
+    rate.id = "rate";
+    rate.innerText = "Average rate: "+ this.averagerate;
+    post.appendChild(rate);
+
+    const username = document.createElement("BUTTON");
+    username.id = "username";
+    username.innerText = this.user;
+    post.appendChild(username);
+
+    const description = document.createElement("P");
+    description.id = "description";
+    description.innerText = this.description;
+    post.appendChild(description);
+
+    alert("new post");
+
+
+}
+var newpost = new Post();
+var newpost2 = new Post();
+*/
