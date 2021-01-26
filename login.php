30a17beea51aee031b78744047d88b6ea4a100c3
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-26 20:17:47 +0000

    Add files via upload
    v2 of connecting to a database, pulling from database and login functionality

--- login.php
+++ login.php
@@ -1,39 +1,46 @@
-<!-- LOG IN PAGE -->
-<?php
-  # Access session.
-session_start() ;
-# Open database connection.
-require ( 'connect_db.php' ) ;
-
-?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Post17 by FringeLogic</title>
-        <link rel = "stylesheet"
-            type = "text/css"
-            href = "loginstyle.css" />
-
-        <IMG id="namebanner"; SRC="logo.png"></IMG>
-    </head>
-    <body id="body">
-
-        <script type="text/javascript" src="login.js"></script>
-
-        
-        
-        <button id="loginbutton">login</button>
-        <button id ="createaccountbutton" onclick="openCreateAccount();">create account</button>
-        
-        <form action = "login_action.php" method="post">
-            <label for="email" id="emailtext">Email:</label><br>
-            <input type="text" id="email" name="email"><br>
-            <label for="password" id="passwordtext">Password:</label><br>
-            <input type="text" id="password" name="password"><br>
-            <button id="inputbutton" onclick="checkUserDetails();">Login</button>
-        </form>
-        
-        
-
-    </body>
+<head>
+    <meta charset="utf-8"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<?php
+    require('connect_db.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['email'])) {
+        $email = stripslashes($_REQUEST['email']);    // removes backslashes
+        $email = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM `users` WHERE email='$email'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['email'] = $email;
+            // Redirect to user dashboard page
+            header("Location: dashboard.php");
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect email/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="email" placeholder="Email" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="registration.php">New Registration</a></p>
+  </form>
+<?php
+    }
+?>
+</body>
 </html>
