bc1dfd5ebb8b33205174c53d767cb9f8ecf645c5
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-26 21:04:40 +0000

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,19 +1,8 @@
 <?php
-//include auth_session.php file on all user panel pages
-include("auth_session.php");
+    session_start();
+    // Destroy session
+    if(session_destroy()) {
+        // Redirecting To Home Page
+        header("Location: login.php");
+    }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Dashboard - Client area</title>
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-    <div class="form">
-        <p>Hey, <?php echo $_SESSION['email']; ?>!</p>
-        <p>You are now user dashboard page.</p>
-        <p><a href="logout.php">Logout</a></p>
-    </div>
-</body>
-</html>
