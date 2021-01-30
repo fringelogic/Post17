b9ec45d843638c055d00f63fd73a6835c0604076
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-30 04:30:40 +0000

    test

--- auth_session.php
+++ auth_session.php
@@ -2,7 +2,6 @@
 session_start();
  if (isset($_SESSION['id'])) {
        header("Location: feed.php");
-       exit();
     } else {
     $message = "Please login";
     echo "<script type='text/javascript'>alert('$message');</script>";
