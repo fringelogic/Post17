eb71a18b5c127f61dff5b6a6a5c4105250cd10d2
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-30 04:37:18 +0000

    Update login.php

--- login.php
+++ login.php
@@ -17,7 +17,6 @@ if(isset($_SESSION["id"])) {
 header("Location:feed.php");
 }
 else {
-?>
     <form class="form" method="post" name="login">
         <h1 class="login-title">Login</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
@@ -25,49 +24,9 @@ else {
         <input type="submit" value="Login" name="submit" class="login-button"/>
         <p class="link"><a href="registration.php">New Registration</a></p>
   </form>
-<?php}?>
-
-
-
-
-
-
-    session_start();
-    require("connect_db.php");
-    // When form submitted, check and create user session.
-    username=$_POST['username']; 
-if ( ! empty( $_POST ) ) {
-    if (isset($_POST['username'])) {
-        $username = stripslashes($_REQUEST['username']);    // removes backslashes
-        $username = mysqli_real_escape_string($con, $username); //Prevents SQL injection
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
-        // Check user is exist in the database
-        $query    = "SELECT * FROM `users` WHERE username='$username'
-                     AND password='" . md5($password) . "'";
-        $result = mysqli_query($con, $query) or die(mysqli_error());
-        $rows = mysqli_num_rows($result);
-        if ($rows == 1) {
-            $_SESSION['id'] = $id;
-            // Redirect to user dashboard page
-            header("Location: feed.php");
-            exit;
-        } else {
-            echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
-                  </div>";
-        }
-    } else {
+<?php
+}
 ?>
-    <form class="form" method="post" name="login">
-        <h1 class="login-title">Login</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
-        <input type="password" class="login-input" name="password" placeholder="Password"/>
-        <input type="submit" value="Login" name="submit" class="login-button"/>
-        <p class="link"><a href="registration.php">New Registration</a></p>
-  </form>
-<?php}?>
 
 <!DOCTYPE html>
 <html>
