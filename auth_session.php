b12337f3984746b8210e33d99c192814e79d49d2
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-30 05:24:45 +0000

    Update auth_session.php

--- auth_session.php
+++ auth_session.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
- if (isset($_SESSION['id'])) {
-       header("Location: feed.php");
-    } else {
-    $message = "Please login";
-    echo "<script type='text/javascript'>alert('$message');</script>";
-}
+      if(!isset($_SESSION['id'])) : 
+      header("Location: login.php");  
 ?>
