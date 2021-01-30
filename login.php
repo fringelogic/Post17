ef400150182c4eec5a159b5bd4ee18a62e308abf
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-30 04:25:41 +0000

    test

--- login.php
+++ login.php
@@ -1,8 +1,42 @@
 <?php
+session_start();
+$message="";
+if(count($_POST)>0) {
+ $con = mysqli_connect("localhost","jat4","6;P2Eg4IAZE","jat4"); or die('Unable To connect');
+$result = mysqli_query($con,"SELECT * FROM users WHERE username='" . $_POST["username"] . "' and password = '". $_POST["password"]."'");
+$row  = mysqli_fetch_array($result);
+if(is_array($row)) {
+$_SESSION["id"] = $row['id'];
+$_SESSION["username"] = $row['name'];
+$_SESSION["email"] = $row['email'];
+} else {
+$message = "Invalid Username or Password!";
+}
+}
+if(isset($_SESSION["id"])) {
+header("Location:feed.php");
+}
+else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="registration.php">New Registration</a></p>
+  </form>
+<?php}?>
+
+
+
+
+
+
     session_start();
     require("connect_db.php");
     // When form submitted, check and create user session.
-    $username=$_POST['username']; 
+    username=$_POST['username']; 
+if ( ! empty( $_POST ) ) {
     if (isset($_POST['username'])) {
         $username = stripslashes($_REQUEST['username']);    // removes backslashes
         $username = mysqli_real_escape_string($con, $username); //Prevents SQL injection
@@ -14,7 +48,7 @@
         $result = mysqli_query($con, $query) or die(mysqli_error());
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
-            $_SESSION['username'] = $username;
+            $_SESSION['id'] = $id;
             // Redirect to user dashboard page
             header("Location: feed.php");
             exit;
