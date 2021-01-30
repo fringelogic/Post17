17bd64b74b2eff7e545ea63c5ca70fefe599018d
Patryk Pierzgalski <50139349+Zynthetik@users.noreply.github.com>
2021-01-30 04:26:38 +0000

    test

--- auth_session.php
+++ auth_session.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
- if (isset($_SESSION['username'])) {
-        /// your code here
+ if (isset($_SESSION['id'])) {
+       header("Location: feed.php");
+       exit();
     } else {
     $message = "Please login";
     echo "<script type='text/javascript'>alert('$message');</script>";
